1d3f82c48306bf332d4d51fd2e31aa5d745fbfc2
Niko Lagman <Lagman.Niko@gmail.com>
2017-05-19 13:32:01 -0400

    Clean up main.cpp for readability

--- main.cpp
+++ main.cpp
@@ -2,52 +2,59 @@
 #include "scheduler.cpp"
 using namespace std;
 
+/*
+  Algorithm for scheduling.
+  0: first come first serve,
+  1: shortest job next,
+  2: priority
+*/
+static int algorithm = 0;
+
 void displaySchedule (Scheduler &);
 
 int main () {
   cout << endl;
 
   std::vector<Process> pending;
-
   Process p0;
+  Process p1;
+  Process p2;
+  Process p3;
+  Process p4;
+  Scheduler scheduler;
+
+
+  /* Pending Processes*/
   p0.setNumber(0);
   p0.setBurstTime(3);
   p0.setArrivalTime(0);
   p0.setPriority(0);
-
-  Process p1;
   p1.setNumber(1);
   p1.setBurstTime(5);
   p1.setArrivalTime(3);
   p1.setPriority(0);
-
-  Process p2;
   p2.setNumber(2);
   p2.setBurstTime(1);
   p2.setArrivalTime(5);
   p2.setPriority(0);
-
-  Process p3;
   p3.setNumber(3);
   p3.setBurstTime(2);
   p3.setArrivalTime(6);
   p3.setPriority(0);
-
-  Process p4;
   p4.setNumber(4);
   p4.setBurstTime(7);
   p4.setArrivalTime(9);
   p4.setPriority(0);
-
   pending.push_back(p0);
   pending.push_back(p1);
   pending.push_back(p2);
   pending.push_back(p3);
   pending.push_back(p4);
+  /* Pending Processes*/
 
-  Scheduler scheduler;
-  scheduler.schedule(0, pending);
-
+  /* Send processes to scheduler */
+  scheduler.schedule(algorithm, pending);
+  /* Display schedule queue */
   displaySchedule(scheduler);
 
   cout << endl;
@@ -55,7 +62,14 @@ int main () {
 }
 
 void displaySchedule (Scheduler & scheduler) {
+  // Three space columns.
+  cout << "#   " << "AT  " << "BT  " << "P   " << endl;
+  cout << "----------------" << endl;
   for (int i = 0; i < scheduler.getSize(); i++) {
-    cout << scheduler.get(i).getArrivalTime() << endl;
+    cout << i << "   ";
+    cout << scheduler.get(i).getArrivalTime() << "   ";
+    cout << scheduler.get(i).getBurstTime() << "   ";
+    cout << scheduler.get(i).getPriority() << "   ";
+    cout << endl;
   }
 }
--- scheduler.cpp
+++ scheduler.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <iostream>
 #include "process.cpp"
 using namespace std;
 
