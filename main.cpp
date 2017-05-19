39abaec29aaef246a5f61a7884506a4d31d449a3
Niko Lagman <Lagman.Niko@gmail.com>
2017-05-19 13:21:38 -0400

    Fix bug in fcfs() in Scheduler class

--- main.cpp
+++ main.cpp
@@ -1,22 +1,61 @@
 #include <iostream>
 #include "scheduler.cpp"
-
 using namespace std;
+
+void displaySchedule (Scheduler &);
 
 int main () {
   cout << endl;
 
-  Process process;
-  process.setNumber(1);
-  cout << "Process @ " << & process << ", number: " << process.getNumber();
-  
-  cout << endl;
+  std::vector<Process> pending;
+
+  Process p0;
+  p0.setNumber(0);
+  p0.setBurstTime(3);
+  p0.setArrivalTime(0);
+  p0.setPriority(0);
+
+  Process p1;
+  p1.setNumber(1);
+  p1.setBurstTime(5);
+  p1.setArrivalTime(3);
+  p1.setPriority(0);
+
+  Process p2;
+  p2.setNumber(2);
+  p2.setBurstTime(1);
+  p2.setArrivalTime(5);
+  p2.setPriority(0);
+
+  Process p3;
+  p3.setNumber(3);
+  p3.setBurstTime(2);
+  p3.setArrivalTime(6);
+  p3.setPriority(0);
+
+  Process p4;
+  p4.setNumber(4);
+  p4.setBurstTime(7);
+  p4.setArrivalTime(9);
+  p4.setPriority(0);
+
+  pending.push_back(p0);
+  pending.push_back(p1);
+  pending.push_back(p2);
+  pending.push_back(p3);
+  pending.push_back(p4);
 
   Scheduler scheduler;
-  std::vector<Process> queue;
-  queue.push_back(process);
-  scheduler.schedule(0, queue);
+  scheduler.schedule(0, pending);
+
+  displaySchedule(scheduler);
 
   cout << endl;
   return 0;
+}
+
+void displaySchedule (Scheduler & scheduler) {
+  for (int i = 0; i < scheduler.getSize(); i++) {
+    cout << scheduler.get(i).getArrivalTime() << endl;
+  }
 }
--- scheduler.cpp
+++ scheduler.cpp
@@ -3,16 +3,19 @@
 #include "process.cpp"
 using namespace std;
 
-#ifndef SCHEDULER_H
-#define SCHEDULER_H
+#ifndef SCHEDULER
+#define SCHEDULER
 
 class Scheduler {
 public:
   Scheduler ();
   ~Scheduler ();
   void schedule (int, std::vector<Process>);
+  Process get (int);
+  int getSize ();
 private:
   std::vector<Process> queue;
+  Process nullProcess;
   void fcfs (std::vector<Process>);
   void sjn (std::vector<Process>);
   void priority (std::vector<Process>);
@@ -27,11 +30,19 @@ Scheduler::~Scheduler () {
   return;
 }
 
+Process Scheduler::get (int i) {
+  if (i < 0 || i > queue.size()) return nullProcess;
+  return queue.at(i);
+}
+
+int Scheduler::getSize () {
+  return queue.size();
+}
+
 /* int algorithm - 0: first come first serve, 1: shortest job next, 2: priority */
 void Scheduler::schedule (int algorithm, std::vector<Process> processes) {
   queue.clear();
   queue.swap(queue);
-  cout << "Address of incoming processes: " << & processes;
   switch (algorithm) {
     case 0:
       fcfs (processes);
@@ -49,6 +60,20 @@ void Scheduler::schedule (int algorithm, std::vector<Process> processes) {
 }
 
 void Scheduler::fcfs (std::vector<Process> processes) {
+  // Loop to add processes.
+  int processesSize = processes.size();
+  for (int i = 0; i < processesSize ; i++) {
+    int earliest = -1;
+    // Loop to find earliest process.
+    for (int j = 0; j < processes.size(); j++) {
+      // If we just started or if the current process has earlier arrival time then the previous earliest.
+      if (earliest == -1 || processes.at(j).getArrivalTime() < processes.at(earliest).getArrivalTime())
+        earliest = j;
+    }
+    // Add to queue, remove from pending list.
+    queue.push_back( processes.at(earliest) );
+    processes.erase( processes.begin() + earliest );
+  }
   return;
 }
 void Scheduler::sjn (std::vector<Process> processes) {
